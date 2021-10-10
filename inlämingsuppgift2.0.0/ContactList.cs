1f56aac24140d062cdc4cdaeade69835feaf7965
97mahyus <97mahyus@MS713797>
2021-10-11 00:02:26 +0200

    inlämning2

---
+++ inlämingsuppgift2.0.0/ContactList.cs
@@ -0,0 +1,16 @@
+﻿namespace inlämingsuppgift2._0._0
+{
+    internal class ContactList
+    {
+        public ContactList()
+        {
+             public List<Person> MyContacts = new()
+
+
+    public void AddPersonToContactList(Person personToAdd)
+        {
+            this.MyContacs.Add(personToAdd);
+        }
+    }
+}
+}
